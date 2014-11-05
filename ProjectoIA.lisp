a37c89e92a4ca78a8d99e3fcf4622002de895705
Antonio Manuel da Silva Ferreira <ist175787@linbase.rnl.tecnico.ulisboa.pt>
2014-11-05 18:53:28 +0000

    Quase Ta Quase

--- ProjectoIA.lisp
+++ ProjectoIA.lisp
@@ -10,24 +10,46 @@
 
 (defvar variavel :string)	;ver isto
 
-(defstruct (restricao (:constructor cria-restricao)
-						(:type list))
+(defstruct (restricao (:constructor cria-restricao) (:type list))
 		(variaveis :type list)
 		(funcao-validacao?))
 
 
 
 (defstruct (psr (:constructor cria-psr))
-
-		(lista-variareis :type list)
+		(lista-variaveis :type list)
 		(lista-dominios :type list)
-		(lista-restricoes :type list)				
+		(lista-restricoes :type list)		
+		atribuicoes ;;Recebe um PSR, retorna a lista de atribuicoes
+		variaveis-todas
+		variaveis-nao-atribuidas
+		variavel-valor
+		variavel-dominio
+		variavel-restricoes
+		adiciona-atribuicao!
+		remove-atribuicao!
+		altera-dominio!
+		completo-p
+		consistente-p
+		variavel-consistente-p
+		atribuicao-consistente-p
+		atribuicoes-consistentes-arco-p
 )
 
+(defun atribuicoes1 (p) 
+		(mapcar #'cons 
+			(psr-lista-variaveis p) (car(psr-lista-dominios p))
+			
+		)			
+)
+
+(defun variaveis-todas1 (p) 
+		(lista-variaveis p)
+)
+
+(defun variaveis-nao-atribuidas (p)
 
 ; Tipo PSR
-
-
 
 
 

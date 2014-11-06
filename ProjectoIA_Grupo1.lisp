bcad628b2d939fa26579b3c28f3ea3195348f0d3
Beatriz Santos <beatriz.vbps@gmail.com>
2014-11-06 19:54:57 +0000

    ja passamos a um teste

--- ProjectoIA_Grupo1.lisp
+++ ProjectoIA_Grupo1.lisp
@@ -2,42 +2,34 @@
 
 
 ;;estrutura que define o tipo restricao
-(defstruct (restricao )
-		listaVariaveis 
+(defstruct (restricao)
+		variaveis 
 		funcao-validacao)
 
 		
 ;construtor que recebe uma lista das variaveis envolvidades na restricao e um predico e retorna a restricao correspondente
 (defun cria-restricao(lista-variaveis predicado)
-	(make-restricao :listaVariaveis lista-variaveis :funcao-validacao? predicado))
-
-(defun restricao-variaveis (res)
-	(restricao-lista-variaveis res))
-	
-(defun restricao-funcao-validacao (res)
-	(restricao-funcao-validacao res))
-
+	(make-restricao :variaveis lista-variaveis :funcao-validacao predicado))
 
 	
 (defstruct (psr)
 		lista-variaveis
 		lista-dominios 
 		lista-restricoes 
-		;;lista-atribuicoes	
 		hash-atribuicoes ;;hash table com os pares atribuicoes
 )
 
 (defun cria-psr (vars dominios restricoes)
-	(setq *meu-hash* (make-hash-table))
-	(dotimes (i 10) (setf (gethash i *meu-hash*) i))
-	(make-psr :lista-variaveis vars :lista-dominios dominios :lista-restricoes restricoes :hash-atribuicoes *meu-hash*))
+	;;(setq hashtable-atribuicoes (make-hash-table))
+	;;(dotimes (i 4) (setf (gethash i *hashtable-atribuicoes*) i))  ;;;;nao por isto a por valores ja
+	(make-psr :lista-variaveis vars :lista-dominios dominios :lista-restricoes restricoes :hash-atribuicoes (make-hash-table)))
 
 (defun psr-atribuicoes (p)
-	(let (lista_retornada '())
+	(let ((lista-retornada NIL))
 		(maphash #'(lambda (key value) 
-					(setf lista_retornada(append lista_retornada(list(cons key value))))) 
-					*meu-hash*)
-		lista_retornada))
+					(setf lista-retornada(append lista-retornada(list(cons key value))))) 
+					(psr-hash-atribuicoes p))
+		lista-retornada))
 	
 	
 (defun psr-variaveis-todas (p)
@@ -46,11 +38,68 @@
 
 
 (defun psr-variaveis-nao-atribuidas (p)
-	(car '(psr-lista-variaveis p))
-	(maphash #'(lambda (key value) 
-		(if (nth-value 1 (gethash key *meu-hash*)) (list(cons key NIL)) ()))))
+	(let ((lista-retornada NIL))
+		(dolist (x (psr-lista-variaveis p))
+			(if (gethash x (psr-hash-atribuicoes p))
+				()
+				(setf lista-retornada(append lista-retornada(list x)))
+			)
+		)
+		lista-retornada))
+		
+(defun psr-variavel-valor (p var)
+	(first (list(gethash var (psr-hash-atribuicoes p))))
+)
 	
+(defun psr-variavel-dominio (p var)
+	(let ((vars (psr-lista-variaveis p))
+			(doms (psr-lista-dominios p))
+			(i 0))
+		(dotimes (x (length vars) i)	
+			(if (equal var (nth i vars))
+				(return)
+				(incf i)
+				
+			) 
+		)
+	(nth i doms))
+)
+
+(defun psr-variavel-restricoes (p var)
+	(let ((vars (psr-lista-variaveis p))
+			(restrs (psr-lista-restricoes p))
+			(i 0))
+		(dotimes (x (length vars) i)	
+			(if (equal var (nth i vars))
+				(return)
+				(incf i)
+				
+			) 
+		)
+	(nth i restrs))
+)
+
 	
-	
-	
-	
+(defun psr-adiciona-atribuicao! (p var val)
+	(setf (gethash var (psr-hash-atribuicoes p)) val)
+)
+
+(defun psr-remove-atribuicao! (p var)
+	(remhash var (psr-hash-atribuicoes p))
+)
+
+(defun psr-altera-dominio! (p var dom)
+	(let ((vars (psr-lista-variaveis p))
+		  (i 0))
+		(dotimes (x (length vars) i)	
+			(if (equal var (nth i vars))
+				(return)
+				(incf i)
+				
+			) 
+		)
+	(setf (nth i (psr-lista-dominios p)) dom))
+)	
+
+
+; (setf p1 (cria-psr '(A B C X Y Z) '('(0 1) '(2 3) '(4 5) '(6 7) '(8 9) '(1 4)) NIL))
